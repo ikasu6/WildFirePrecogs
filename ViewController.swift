f7abcbacc57161bc92ca14489f424cc7019ab9db
Ishwarya Kasu <ikasu@uw.edu>
2024-02-28 19:03:10 -0800

    Added  functionalities 1) bounding box turning green when same rectangle is detected for consecutive readings. 2) Automatic Image Capture: Once it bounding box turns green indicating same rectangle is getting detected continously then do presepective correction and save the resultant image.

--- ViewController.swift
+++ ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         self.setCameraInput()
         self.showCameraFeed()
         self.setCameraOutput()
-        //setupCaptureButton()
+        setupCaptureButton()
        
         
     }
@@ -58,6 +58,29 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         self.previewLayer.videoGravity = .resizeAspectFill
         self.view.layer.addSublayer(self.previewLayer)
         self.previewLayer.frame = self.view.frame
+    }
+    
+    func setupCaptureButton() {
+        let captureButton = UIButton(type: .system) // Creates a new UIButton with the system type, which gives it a default style.
+        captureButton.setTitle("Capture", for: .normal) // Sets the button title.
+        captureButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20) // Optional: Sets the font of the button title.
+        captureButton.backgroundColor = UIColor.systemBlue // Sets the background color of the button.
+        captureButton.setTitleColor(UIColor.white, for: .normal) // Sets the title color.
+        captureButton.layer.cornerRadius = 25 // Optional: Sets the corner radius to make the button rounded.
+        
+        captureButton.addTarget(self, action: #selector(doScan(sender:)), for: .touchUpInside) // Adds an action to the button.
+        
+        self.view.addSubview(captureButton) // Adds the button to the view hierarchy.
+        
+        captureButton.translatesAutoresizingMaskIntoConstraints = false // Disables autoresizing mask translation into Auto Layout constraints.
+        
+        // Constraints for the button
+        NSLayoutConstraint.activate([
+            captureButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -20), // Positions the button 20 points from the bottom safe area.
+            captureButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor), // Centers the button horizontally.
+            captureButton.widthAnchor.constraint(equalToConstant: 200), // Sets the button width.
+            captureButton.heightAnchor.constraint(equalToConstant: 50) // Sets the button height.
+        ])
     }
     
     
@@ -119,7 +142,10 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
             }
         })
         
-        //Setting the Parameters of VNrectangle detection request
+        
+        
+        
+    
         request.minimumAspectRatio = VNAspectRatio(1.3)
         request.maximumAspectRatio = VNAspectRatio(1.6)
         request.minimumSize = Float(0.5)
@@ -134,14 +160,18 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     
     
     private func detectRectangle(in image: CVPixelBuffer) {
-        // Configuring  the rectangle detection request
+        // Configure the rectangle detection request
         let rectangleDetectionRequest = VNDetectRectanglesRequest { [weak self] request, error in
             guard let self = self else { return }
             DispatchQueue.main.async {
                 self.processDetectionResults(request.results, in: image)
+                
+                
             }
         }
         configureDetectionRequest(rectangleDetectionRequest)
+        
+        // Perform the request on the given image
         performDetection(with: rectangleDetectionRequest, on: image)
     }
 
@@ -163,11 +193,10 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     private var noChangeCounter: Int = 0 // Counter for consecutive readings without significant change
     private let noChangeThreshold: Int = 15 // Threshold for changing the bounding box color
     private var shouldDrawGreenBoundingBox: Bool = false // Flag to indicate bounding box color
-    private var autoCaptureEnabled: Bool = true
 
 
     private func processDetectionResults(_ results: [Any]?, in image: CVPixelBuffer) {
-        // any rectangle observations
+        // Check if there are any rectangle observations
         guard let observations = results as? [VNRectangleObservation], !observations.isEmpty else {
             DispatchQueue.main.async { [weak self] in
                 self?.removeMask() // Clear existing bounding box if no rectangles are detected
@@ -176,7 +205,7 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
             return
         }
         
-        // logic to proceed if there's at least one detected rectangle
+        // Proceed if there's at least one detected rectangle
         if let firstRectangle = observations.first {
                 if hasRectangleChanged(rectangle: firstRectangle) {
                     noChangeCounter = 0 // Reset counter on change
@@ -185,7 +214,6 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
                     noChangeCounter += 1 // Increment counter if no change
                     if noChangeCounter >= noChangeThreshold {
                         shouldDrawGreenBoundingBox = true // Indicate to draw bounding box in green
-                        performAutomaticCapture(with: firstRectangle, from: image) // Trigger automatic capture
                     }
                 }
                 DispatchQueue.main.async { [weak self] in
@@ -197,46 +225,16 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         }
     }
     
-    private func performAutomaticCapture(with rectangleObservation: VNRectangleObservation, from buffer: CVImageBuffer) {
-        guard shouldDrawGreenBoundingBox, autoCaptureEnabled else {
-            return
-        }
-
-        autoCaptureEnabled = false // to prevent further captures until  reset
-
-        // to do perspective correction and save the image
-        DispatchQueue.main.async {
-            self.doPerspectiveCorrection(rectangleObservation, from: buffer)
-            //  popup notification
-            self.showAlertWith(title: "Image Captured", message: "The image has been automatically captured and saved.")
-            
-            // Resetting autoCaptureEnabled flag after a delay to allow for new captures
-            DispatchQueue.main.asyncAfter(deadline: .now() + 5) { // 5 seconds delay
-                self.autoCaptureEnabled = true
-                self.removeMask()
-                //sleep(5)
-                self.resetDetectionState()
-                
-            }
-        }
-    }
     
-    
-    private func resetDetectionState() {
-        self.lastDetectedRectangle = nil
-        self.noChangeCounter = 0
-        self.shouldDrawGreenBoundingBox = false
-        self.autoCaptureEnabled = true // Ensure this is ready for the next detection cycle
-    }
     
     private func hasRectangleChanged(rectangle: VNRectangleObservation) -> Bool {
         guard let lastRectangle = lastDetectedRectangle else {
             return true // No last rectangle, so consider this as changed
         }
-        // Implementing comparison logic here.
+        // Implement your comparison logic here. This is a simple example that checks if the centers are significantly different.
         let lastCenter = lastRectangle.boundingBox.origin
         let currentCenter = rectangle.boundingBox.origin
-        let distanceThreshold: CGFloat = 0.2 // Adjust based on your needs
+        let distanceThreshold: CGFloat = 0.1 // Adjust based on your needs
         let distance = hypot(lastCenter.x - currentCenter.x, lastCenter.y - currentCenter.y)
         return distance > distanceThreshold
     }
@@ -244,15 +242,18 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
 
     private func shouldUpdateBoundingBox(for newRectangle: VNRectangleObservation) -> Bool {
         guard let lastRectangle = lastDetectedRectangle else {
-            return true
+            return true // Always update if there's no previous rectangle
         }
         
-
+        // Implement comparison logic here. This is a placeholder for a simple comparison.
+        // You might want to compare the center points, the area, or the corners of the rectangles.
         return !areRectanglesSimilar(rect1: lastRectangle, rect2: newRectangle)
     }
 
     private func areRectanglesSimilar(rect1: VNRectangleObservation, rect2: VNRectangleObservation) -> Bool {
-
+        // This function should return true if the rectangles are considered similar.
+        // For simplicity, let's just check if the center points are approximately equal.
+        // You can adjust the tolerance and comparison criteria based on your requirements.
         let tolerance: CGFloat = 0.2 // Adjust based on your needs
         let a = abs(rect1.bottomLeft.x - rect2.bottomLeft.x)
         let b = abs(rect1.topRight.x - rect2.topRight.x)
@@ -269,13 +270,13 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
 
     
 //    func drawBoundingBox(rect : VNRectangleObservation) {
-//
+//    
 //        let transform = CGAffineTransform(scaleX: 1, y: -1).translatedBy(x: 0, y: -self.previewLayer.frame.height)
 //        let scale = CGAffineTransform.identity.scaledBy(x: self.previewLayer.frame.width, y: self.previewLayer.frame.height)
 //
 //        let bounds = rect.boundingBox.applying(scale).applying(transform)
 //        createLayer(in: bounds)
-//
+//        
 //
 //    }
     
@@ -309,7 +310,7 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
 //        maskLayer.opacity = 0.75
 //        maskLayer.borderColor = UIColor.red.cgColor
 //        maskLayer.borderWidth = 5.0
-//
+//        
 //        previewLayer.insertSublayer(maskLayer, at: 1)
 //
 //    }
@@ -356,9 +357,7 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     
     func showAlertWith(title: String, message: String) {
         let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
-            self.resetDetectionState() // Reset when the user acknowledges
-        }))
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
         present(ac, animated: true)
     }
     
